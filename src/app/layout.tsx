9a7a22219bed53c9f596a99459467fdb27697574
flxthesixth <flxthesixth@users.noreply.github.com>
2026-09-19 17:59:37 +0800

    design: adopt SHIREN neo-brutalist collector layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="16x16" href="/icon-256.png" />
         <link rel="shortcut icon" href="/icon-256.png" />
         <link rel="apple-touch-icon" href="/icon-256.png" />
-        <meta name="theme-color" content="#f6f4ef" />
+        <meta name="theme-color" content="#c9f422" />
       </head>
       <body className={vazirmatn.className}>
         <AudioProvider>{children}</AudioProvider>
